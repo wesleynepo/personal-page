6ed349f3cb236485e0ca3f3b9fba654bcc26b9a4
wesley.cardoso <wesley.cardoso@gazin.com.br>
2021-09-24 22:22:50 -0300

    feat: remove ununsed

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,22 @@ import {
   ChakraProvider,
   Box,
   Text,
-  Link,
-  VStack,
-  Code,
   Grid,
   theme,
+  Spinner,
+  Center
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
+      <Grid minH="100vh">
+        <Center>
+          <Spinner size="xl" m="5"/>
           <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+            Working progress!
           </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
+        </Center>
       </Grid>
     </Box>
   </ChakraProvider>
