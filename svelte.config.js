f8d33ef0bde97708c068ef2224fcf5b9f8b1e7e6
wesleynepo <wesleynepomuceno1@gmail.com>
2026-04-18 15:11:41 -0300

    feat: updatae

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,10 @@ const config = {
 			pages: 'build',
 			assets: 'build',
 			fallback: null
-		})
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/personal-page' : ''
+		}
 	}
 };
 
